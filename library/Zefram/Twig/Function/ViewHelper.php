d6dde4468ec8f9080ffe5d905efa150f515e9095
xemlock <xemlock@gmail.com>
2014-12-15 11:41:09 +0100

    view helper can be an instance of any class, the only requirement is that a matching method is available

--- library/Zefram/Twig/Function/ViewHelper.php
+++ library/Zefram/Twig/Function/ViewHelper.php
@@ -8,13 +8,19 @@ class Zefram_Twig_Function_ViewHelper extends Zwig_Function_ViewHelper
     protected $safe;
 
     /**
-     * @param string $name
-     * @param Zend_View_Helper_Interface $helper
-     * @param string|array $safe
+     * @param  string $name
+     * @param  object $helper
+     * @param  string|array $safe
+     * @throws InvalidArgumentException
      */
-    public function __construct($name, Zend_View_Helper_Interface $helper, $safe = null)
+    public function __construct($name, $helper, $safe = null)
     {
-        parent::__construct($name, $helper);
+        if (!is_object($helper) || !method_exists($helper, $name)) {
+            throw new InvalidArgumentException('The object provided cannot be used as a view helper');
+        }
+
+        $this->name = $name;
+        $this->helper = $helper;
 
         if (null !== $safe) {
             $this->safe = (array) $safe;
